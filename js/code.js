432110b803ea61c2093d4d218f4e4f9f274af992
prostoEM <emran01@yandex.ru>
2021-07-26 23:20:53 +0300

    v2.0

--- js/code.js
+++ js/code.js
@@ -4,7 +4,7 @@ function move(){
     let time = setInterval (load_html, 10)
         function load_html(){
           
-            if (width>=70) {
+            if (width>=100) {
                 clearInterval(time);
                  }
             else {
@@ -21,7 +21,7 @@ function move1(){
     let width = 1
     let time = setInterval (load_html, 10)
         function load_html(){
-            if (width>=30) {
+            if (width>=35) {
                 clearInterval(time);
                  }
             else {
@@ -37,7 +37,7 @@ function move2(){
     let width = 1
     let time = setInterval (load_html, 10)
         function load_html(){
-            if (width>=10) {
+            if (width>=50) {
                 clearInterval(time);
                  }
             else {
@@ -53,7 +53,7 @@ function move3(){
     let width = 1
     let time = setInterval (load_html, 10)
         function load_html(){
-            if (width>=2) {
+            if (width>=10) {
                 clearInterval(time);
                  }
             else {
@@ -70,3 +70,48 @@ move();
 move1();
 move2();
 move3();
+
+
+function buttonTell() {
+    document.getElementById('btn').innerHTML = `
+  8 (915)-251-23-03
+  `;
+
+}
+
+$(document).ready(function(){
+    $("#button_call").on("click", function(){
+        $('html').animate({scrollTop: $('#call').offset().top}, 1000);
+    });
+    $("#button_about").on("click", function(){
+        $('html').animate({scrollTop: $('#about').offset().top}, 1000);
+    });
+    $("#button_skill").on("click", function(){
+        $('html').animate({scrollTop: $('#skill').offset().top}, 1000);
+    });
+    $("#button_portfolio").on("click", function(){
+        $('html').animate({scrollTop: $('#portfolio').offset().top}, 1000);
+    });
+    
+    
+})
+
+jQuery(function($) {
+    $(window).scroll(function(){
+        if($(this).scrollTop()>60){
+            $('#top').addClass('fixed');
+            $('.nav__button').css({
+                'background-color': 'none',
+            })
+        }
+        else if ($(this).scrollTop()<60){
+            $('#top').removeClass('fixed');
+        }
+        
+        
+    });
+});
+
+
+
+
